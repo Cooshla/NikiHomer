739ec0d52dc41af3ef2b0938affb790c1192a55b
SShaw1985 <stephen.shaw85@gmail.com>
2016-11-04 08:47:02 +0000

    initial commit
    MVC web api and umbraco

---
+++ NikiHomer/NikiHomer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NikiHomer.Startup))]
+namespace NikiHomer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
