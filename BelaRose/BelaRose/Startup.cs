6d1c80915fc84f1c11cee6d21fc8f4bf47bafb66
SShaw1985 <stephen.shaw85@gmail.com>
2016-12-14 12:21:57 +0000

    initial commit

---
+++ BelaRose/BelaRose/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BelaRose.Startup))]
+namespace BelaRose
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
